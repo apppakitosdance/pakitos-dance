e16655455ca86e8daf01b4ffdb72d24fdfdeb341
apppakitosdance <anonimatoverdade607@gmail.com>
2025-09-15 22:38:58 -0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,22 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/pakitos-dance' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pakitos-dance/' : '',
-  experimental: {
-    esmExternals: 'loose',
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
   },
 }
 
-export default nextConfig
+export default config
+
 
