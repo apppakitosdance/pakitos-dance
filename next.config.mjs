18b6999a93a0ab4bd958e1cdba8dee157f522a9b
Training-Site <131490722+Nunes025@users.noreply.github.com>
2025-10-03 11:18:33 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,9 +13,6 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/pakitos-dance' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/pakitos-dance' : '',
-  experimental: {
-    esmExternals: 'loose',
-  },
 }
 
 export default nextConfig
